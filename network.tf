c3083224c66ca849fb7fc307de4548025e2bcced
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 19:02:29 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,9 +1,9 @@
-resource ibm_is_vpc "vpc" {
-  name = "myvpc"
+resource ibm_is_vpc "vpc1" {
+  name = "myvpc1"
   }
 
-resource ibm_is_vpc "vpc" {
-  name = "myvpc"
+resource ibm_is_vpc "vpc2" {
+  name = "myvpc2"
 }
 
 resource "ibm_resource_group" "resourceGroup" {
