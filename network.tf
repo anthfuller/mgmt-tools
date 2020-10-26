b5f34e2c0b7d3f3a075f9fcbc42d78dbd9aa84d7
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 19:09:11 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -7,7 +7,7 @@ resource ibm_is_vpc "vpc2" {
    }
 
 resource "ibm_tg_gateway" "new_tg_gw"{
-  name      = "front-office-tg"
+  name      = "terraform-office-tg"
   location  = "east-us"
   global    =  true
   resource_group="6664a071c0b546deb4703269b54a5d9a"
