410b7c8065a78c3f3ff06e00c585f6f497b48ac5
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-27 15:27:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,19 @@
 variable "project_name" {}
 variable "environment" {}
 
+variable "ssh_keyname" {
+  default = ""
+}
+
+# default to using CentOS 8
+variable "image_name" {
+  default = "ibm-centos-8-2-minimal-amd64-1"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+}
+
 variable "resource_group" {
   default = "Default"
 }
