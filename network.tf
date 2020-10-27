8c105cd0810d6e83e9cbe1909726132786181c76
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 23:02:09 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-resource ibm_is_vpc "mgmt_tools_vpc" {
+resource ibm_is_vpc "iac_iks_vpc" {
   name = "${var.project_name}-${var.environment}-vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
@@ -11,11 +11,11 @@ resource "ibm_tg_gateway" "new_tg_gw"{
   resource_group="64474a4706f64480a1a25926de5f12dd"
  }
 
-resource "ibm_is_subnet" "mgmt_tools_subnet" {
+resource "ibm_is_subnet" "iac_iks_subnet" {
   count                    = local.max_size
   name                     = "${var.project_name}-${var.environment}-subnet-${format("%02s", count.index)}"
   zone                     = var.vpc_zone_names[count.index]
-  vpc                      = ibm_is_vpc.mgmt_tools_vpc.id
+  vpc                      = ibm_is_vpc.iac_iks_vpc.id
   ipv4_cidr_block          = "172.26.${format("%01s", count.index)}.0/26"
   # total_ipv4_address_count = 64
   resource_group           = data.ibm_resource_group.group.id
