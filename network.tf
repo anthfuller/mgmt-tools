e16804b07fd1b9cd3578ea77412aafdf345c025d
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-27 00:07:28 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -11,11 +11,4 @@ resource "ibm_is_subnet" "mgmt_subnet" {
   ipv4_cidr_block          = "172.16.0.0/1"
   resource_group           = data.ibm_resource_group.group.id
    }
-
-resource "ibm_tg_gateway" "new_tg_gw"{
-  name      = "mgmt-tg"
-  location  = "eu-de"
-  global    =  true
-  resource_group="cf1794131ebd43efa0c468d35af45849"
-   }
 }
