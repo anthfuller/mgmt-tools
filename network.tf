a5260d606b9499d0c3052699c4afb11c5e9a7742
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 22:00:45 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,14 +1,14 @@
-resource ibm_is_vpc "vpc1" {
-  name = "front-office-dev1"
+resource ibm_is_vpc "vpc" {
+  name = "mgmt-tools-prod"
   }
 
 resource ibm_is_vpc "vpc2" {
   name = "front-office-dev2"
    }
 
-resource "ibm_tg_gateway" "new2_tg_gw"{
-  name      = "front-office-dev2-tg"
-  location  = "us-east"
+resource "ibm_tg_gateway" "new_tg_gw"{
+  name      = "mgmt-tools-prod-tg"
+  location  = "eu-de"
   global    =  true
-  resource_group="6664a071c0b546deb4703269b54a5d9a"
+  resource_group="64474a4706f64480a1a25926de5f12dd"
 }
