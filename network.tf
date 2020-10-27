8e2fc98f8ded632893f138d2b434960f9c456ec6
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 23:45:34 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,11 +1,11 @@
 resource "ibm_is_vpc" "mgmt_tools_vpc" {
-  name = "mgmt-tools-vpc"
+  name = "mgmt_tools_vpc"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
 }
 
 resource "ibm_is_subnet" "mgmt_tools_subnet" {
-  name                     = "mgmt_subnet"
+  name                     = "mgmt1"
   vpc                      = ibm_is_vpc.mgmt_tools_vpc.id
   zone                     = "eu-de-1"
   ipv4_cidr_block          = "172.16.0.0/1"
