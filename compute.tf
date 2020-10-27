64dcaf76764334133aa148a715f4a89db59db47f
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-27 18:15:50 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -15,7 +15,7 @@ resource "ibm_is_instance" "mgmt_instance" {
 
   vpc  = ibm_is_vpc.mgmt_vpc.id
   zone = var.zone
-  keys = [ data.ibm_is_ssh_key.mgmt_instance.id, ibm_is_ssh_key.public_key.id ]
+  keys = [ data.ibm_is_ssh_key.mgmt_vpc.id, ibm_is_ssh_key.public_key.id ]
 
   user_data = file("${path.module}/scripts/setup.sh")
 
