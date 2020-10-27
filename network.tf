11cc50004207d9f0950faad8080c3abb831807ae
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 23:58:12 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -6,11 +6,10 @@ resource "ibm_is_vpc" "mgmt-vpc" {
 
 resource "ibm_is_subnet" "mgmt-subnet" {
   name                     = "mgmt"
-  vpc                      = ibm_is_vpc.mgmt-vpc.id
+  vpc                      = ibm_is_vpc.mgmt_vpc.id
   zone                     = "eu-de-1"
   ipv4_cidr_block          = "172.16.0.0/1"
   resource_group           = data.ibm_resource_group.group.id
-  
    }
 
 resource "ibm_tg_gateway" "new_tg_gw"{
