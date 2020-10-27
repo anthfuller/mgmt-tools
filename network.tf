67455999b2db6b37bf78bc7a7a59304c8818e648
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-27 01:14:42 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -9,14 +9,14 @@ resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
   name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
   zone                      = var.vpc_zone_names[count.index]
   vpc                       = ibm_is_vpc.mgmt_vpc.id
-  cidr                      = "10.240.${format("%01s", count.index)}.0/24"
+  cidr                      = "10.243.0.0/18"
 }
 
 resource "ibm_is_subnet" "mgmt_subnet" {
   name                     = "mgmt"
   zone                     = "frankfurt-1"
   vpc                      = ibm_is_vpc.mgmt_vpc.id
-  ipv4_cidr_block          = "10.240.0.0/24"
+  ipv4_cidr_block          = "10.243.0.0/18"
   resource_group           = data.ibm_resource_group.group.id
   depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
