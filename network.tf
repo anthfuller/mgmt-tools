9d2065bb01d1d70567814665f3093625ed44952a
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 20:22:21 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,13 +1,13 @@
 resource ibm_is_vpc "vpc1" {
-  name = "myvpc1"
+  name = "front-office-dev1"
   }
 
 resource ibm_is_vpc "vpc2" {
-  name = "myvpc2"
+  name = "front-office-dev2"
    }
 
 resource "ibm_tg_gateway" "new2_tg_gw"{
-  name      = "terraform-office-tg"
+  name      = "front-office-dev2-tg"
   location  = "us-east"
   global    =  true
   resource_group="6664a071c0b546deb4703269b54a5d9a"
