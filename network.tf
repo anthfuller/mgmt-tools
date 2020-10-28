e3254ba3668e10a58504470af9c1e8431316504e
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-27 21:10:54 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,24 +1,12 @@
 resource "ibm_is_vpc" "mgmt_vpc" {
-  name = "mgmt-vpc"
-  resource_group = data.ibm_resource_group.group.id
-  address_prefix_management = "manual"
-}
-
-resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
-  count                     = local.max_size
-  name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
-  zone                      = var.vpc_zone_names[count.index]
-  vpc                       = ibm_is_vpc.mgmt_vpc.id
-  cidr                      = "10.243.0.0/18"
+  name = "${var.project_name}-${var.environment}-vpc"
 }
 
 resource "ibm_is_subnet" "mgmt_subnet" {
-  name                     = "mgmt"
-  zone                     = "eu-de-1"
-  vpc                      = ibm_is_vpc.mgmt_vpc.id
-  ipv4_cidr_block          = "10.243.0.0/18"
-  resource_group           = data.ibm_resource_group.group.id
-  depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
+  name            = "${var.project_name}-${var.environment}-subnet"
+  vpc             = ibm_is_vpc.mgmt_vpc.id
+  zone            = var.zone
+  total_ipv4_address_count = "64"
 }
 
 resource "ibm_is_security_group" "mgmt_security_group" {
